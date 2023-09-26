47b1d4589f6cd41a93c06eaeff4c8746328817c9
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-26 04:46:35 -0700

    update

--- slist/src/App.js
+++ slist/src/App.js
@@ -1,10 +1,18 @@
 
+import React, {useState} from 'react'
 import './components/index.css'
+import Data from './components/data'
+import List from './components/list'
+
 
 function App() {
+  const [people, setPeople] = useState(Data)
   return (
 
     <>
+    <div className="container">
+      
+    </div>
   <p>what up</p>
     </>
 
--- slist/src/components/data.js
+++ slist/src/components/data.js
@@ -1,36 +1,35 @@
 
-export let list = [ 
-    
+let list = [
     {
     id: 1,
     name: 'johnds',
     age: 33,
-    image: ''
+    image: './Images/angelLooks.png'
     },
     {
         id: 2,
         name: 'johns',
         age: 333,
-        image: ''
+        image: './Images/angelsb.png'
         },
         {
             id: 3,
             name: 'johnd',
             age: 353,
-            image: ''
+            image: './Images/RedHeadAngel.png'
             },
             {
                 id: 4,
                 name: 'johnf',
                 age: 3663,
-                image: ''
+                image: './Images/goldenface.png'
                 },
                 {
                     id: 5,
                     name: 'johne',
                     age: 3223,
-                    image: ''
+                    image: './Images/goldenHarleyQuinn.png'
                     },
-
 ]
 
+export default list;
